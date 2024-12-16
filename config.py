5583507daae277d6de0a5a556144f6b60ec9c832
Jainamroxz <yoginum2@outlook.com>
2024-12-17 03:09:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@
 BOT_TOKEN=("7507854182:AAG17dDLS1Rm761Gmk0pPKzC_R6pfyD99VM")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@Mr_sid_001")
+OWNER_USERNAME=("@God_x_Sikandar")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("@God_x_Sikandar")
+ADMIN_IDS=("1216702307")
 
 
 
